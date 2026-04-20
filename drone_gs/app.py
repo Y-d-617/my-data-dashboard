ebc63ce6f0fb545e06e8327fbe8d34e7bfe746c1
Y-d-617 <3169790786@qq.com>
2026-04-20 09:14:48 +0800

    Update app.py

--- drone_gs/app.py
+++ drone_gs/app.py
@@ -14,13 +14,13 @@ if "app_version" not in st.session_state or st.session_state.app_version != "v3_
     st.session_state.sim = HeartbeatSimulator()
     st.session_state.history = []
     st.session_state.app_version = "v3_folium"
-
+WGS-84
 # --- 侧边栏导航 ---
 st.sidebar.title("🧭 导航控制")
 page = st.sidebar.radio("请选择功能页面", ["航线规划", "飞行监控"])
 
 st.sidebar.divider()
-coord_mode = st.sidebar.radio("坐标系设置", ["WGS-84", "GCJ-02"], index=1)
+coord_mode = st.sidebar.radio("坐标系设置", ["GCJ-02", "WGS-84"], index=1)
 
 # --- 页面1：航线规划 ---
 if page == "航线规划":
